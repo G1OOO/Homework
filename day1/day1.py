9b0113f212aa3903f2dec29e04931ebe9cbb5317
G1OOO <gg.tandashvili@gmail.com>
2024-01-11 15:59:13 +0400

    commit

---
+++ day1/day1.py
@@ -0,0 +1,8 @@
+name = "Giorgi"
+last_name = "Tandashvili"
+age = 13
+height = 160
+weight = 45
+
+
+print("My full name is " + name + " " + last_name + " i am " + str(age) + " years old, my height is " + str(height) + "cm, also my weight is " + str(weight) + "kg.")
