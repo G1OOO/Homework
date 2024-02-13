a864d269beec186195fe6a17b592318f586f4286
G1OOO <gg.tandashvili@gmail.com>
2024-02-13 17:44:56 +0400

    commit

--- day4/homework/saxlebi.py
+++ day4/homework/saxlebi.py
@@ -1,12 +1,9 @@
 from turtle import *
 
-# Set the width of the pen
 width(3)
 
-# Set background color to baby blue
 bgcolor("lightblue")
 
-# Draw the first house and yard
 color("red")
 begin_fill()
 for _ in range(4):
@@ -25,12 +22,10 @@ right(135)
 forward(100)
 end_fill()
 
-# Move to draw the second house
 penup()
 goto(200, 0)
 pendown()
 
-# Draw the second house and yard
 color("red")
 begin_fill()
 for _ in range(4):
@@ -49,12 +44,10 @@ right(135)
 forward(100)
 end_fill()
 
-# Move to draw the third house
 penup()
 goto(-200, -200)
 pendown()
 
-# Draw the third house and yard
 color("red")
 begin_fill()
 for _ in range(4):
@@ -73,7 +66,6 @@ right(135)
 forward(100)
 end_fill()
 
-# Draw the sun
 penup()
 goto(-200, 200)
 pendown()
@@ -82,7 +74,6 @@ begin_fill()
 circle(50)
 end_fill()
 
-# Draw the stars
 penup()
 goto(200, 150)
 pendown()
@@ -105,8 +96,6 @@ for _ in range(5):
     forward(40)
     right(144)
 
-# Hide the turtle
 hideturtle()
 
-# Keep the window open
 mainloop()
