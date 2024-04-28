e16297629f38fc63b5323de611847360a31ee7ac
G1OOO <gg.tandashvili@gmail.com>
2024-04-28 12:38:50 +0400

    commit

--- day57/Homework/script.js
+++ day57/Homework/script.js
@@ -1,4 +1,16 @@
-document.getElementById("privilege").getElementsByTagName("p")[0].textContent = "Admin";
-document.getElementById("color").getElementsByTagName("p")[0].textContent = "Blue";
-document.getElementById("width").getElementsByTagName("p")[0].textContent = "100";
-document.getElementById("height").getElementsByTagName("p")[0].textContent = "200";
+function changeWidth() {
+    var privilege = document.getElementById("privilege").value;
+    var color = document.getElementById("color").value;
+    var second = document.getElementById("second").value;
+    var width = document.getElementById("width").value;
+    var third = document.getElementById("third").value;
+    var height = document.getElementById("height").value;
+    var fourth = document.getElementById("fourth").value;
+    var text = document.getElementById("text").value;
+
+    var infoDiv = document.getElementById("infoDiv");
+    var paragraph = infoDiv.querySelector("p");
+
+    paragraph.innerText = text;
+    infoDiv.style.width = width + "px";
+  }
--- day57/Homework/script2.js
+++ day57/Homework/script2.js
@@ -1,21 +1,25 @@
-var obj1 = {
-    privilege: "Admin",
-    color: "Blue",
-    width: 100,
-    height: 200
-};
+var object1 = {
+    name: "Object 1",
+    property1: "Value 1",
+    property2: "Value 2"
+  };
 
-var obj2 = {
-    privilege: "User",
-    color: "Red",
-    width: 150,
-    height: 250
-};
+  var object2 = {
+    name: "Object 2",
+    property1: "Initial Value",
+    property2: "Initial Value"
+  };
 
-obj1.privilege = "Super Admin";
-obj1.color = "Green";
-obj2.width = 200;
-obj2.height = 300;
+  console.log("Before modification:");
+  console.log("Object 1:", object1);
+  console.log("Object 2:", object2);
 
-console.log("Modified obj1:", obj1);
-console.log("Modified obj2:", obj2);
+  object1.property1 = "New Value 1";
+  object1.property2 = "New Value 2";
+
+  object2.property1 = "Another New Value 1";
+  object2.property2 = "Another New Value 2";
+
+  console.log("After modification:");
+  console.log("Object 1:", object1);
+  console.log("Object 2:", object2);
