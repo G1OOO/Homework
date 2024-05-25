4c9ff3703f94eaf53d6fbe83391d16361b83c890
G1OOO <gg.tandashvili@gmail.com>
2024-05-25 14:59:59 +0400

    commit

---
+++ day62/Classwork/python.py
@@ -0,0 +1,11 @@
+# 1
+for i in range(1, 21):
+    if i % 2 != 0:
+        print(i)
+# 2
+for i in range(20, -1, -1):
+    print(i)
+# 3
+for i in range(20, -1, -1):
+    if i % 2 != 0:
+        print(i)
